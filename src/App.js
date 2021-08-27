8182e00f708c9d9d9812ca295156fa585cfe5efb
Aaron <amramirez1222@gmail.com>
2021-08-26 20:06:26 -1000

    Revert "useState app.js"
    This reverts commit 21f04d3d134e953396a7f1bd9833e569b4f9f4f1.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -18,85 +18,90 @@ import {} from '@fortawesome/free-solid-svg-icons' */
 
 /* This is the driver file. Where all the components come together at the end as well as calling the api with axios */
 
-const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-  
+class App extends Component{
+  state = {
+    users: [],
+    user: {},
+    repos:[],
+    loading: false,
+    alert: null,
+  }
+
+/*   async componentDidMount(){
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({users: res.data, loading: false});
+  } */
 
   //search Github users
-  const searchUsers = async text =>{
-    setLoading(true);
+  searchUsers = async text =>{
+    this.setState({loading: true});
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setUsers(res.data.items);
-    setLoading(false);
+    this.setState({users: res.data.items, loading: false});
   }
 
   //Get single Github user
-  const getUser= async (username) => {
-    setLoading(true);
+  getUser= async (username) => {
+    this.setState({loading: true});
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setUser(res.data);
-    setLoading(false);
+    this.setState({user: res.data, loading: false});
   }
 
   // Get Users Repos
-  const getUserRepos= async username => {
-    setLoading(true);
+  getUserRepos= async username => {
+    this.setState({loading: true});
     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setRepos(res.data);
-    setLoading(false);
+    this.setState({repos: res.data, loading: false});
   }
 
   //clear users from states
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
+  clearUsers = () => this.setState({ users: [], loading: false});
 
-  }  
   //set Alert
-  const showAlert = (msg, type) => {
-    setAlert({msg, type})
-    setTimeout(() => setAlert(null), 5000)
+  setAlert = (msg, type) => {
+    this.setState({ alert: {msg, type}});
+    setTimeout(() => this.setState({alert: null}), 5000)
   }
+
+  render(){
+    const {users, user, repos, loading} = this.state;
     
 
-  return (
-    <Router>
-      <div className="App">
-        <Navbar/>     
-        <div className='container'>
-          <Alert alert={alert} />
-          <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search 
-                  searchUsers={searchUsers} 
-                  clearUsers={clearUsers} 
-                  showClear={users.length > 0 ? true : false} /* this is passed from the search.js to be able to get the text for searchUsers method in here */
-                  setAlert={showAlert}
-                  />
-                <Users loading ={loading} users={users}/> 
-              </Fragment>
-            )} />
-            <Route exact path='/about' component={About}/>
-            <Route exact path='/user/:login' render={props => (
-              <User 
-              {...props} 
-              getUser={getUser} 
-              getUserRepos={getUserRepos}
-              user={user} 
-              repos={repos}
-              loading={loading} 
-              />
-            )} />
-          </Switch>
-        </div>      
-      </div>
-    </Router>
-  );
+    return (
+      <Router>
+        <div className="App">
+          <Navbar/>     
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClear={users.length > 0 ? true : false} /* this is passed from the search.js to be able to get the text for searchUsers method in here */
+                    setAlert={this.setAlert}
+                    />
+                  <Users loading ={loading} users={users}/> 
+                </Fragment>
+              )} />
+              <Route exact path='/about' component={About}/>
+              <Route exact path='/user/:login' render={props => (
+                <User 
+                {...props} 
+                getUser={this.getUser} 
+                getUserRepos={this.getUserRepos}
+                user={user} 
+                repos={repos}
+                loading={loading} 
+                />
+              )} />
+            </Switch>
+          </div>      
+        </div>
+      </Router>
+    );
+  }
 }
 
 
